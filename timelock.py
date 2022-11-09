cc99b0a6255b8a04f54fbebb6cb1244985c9bd13
ethanhebert <80844614+ethanhebert@users.noreply.github.com>
2022-11-09 11:36:57 -0600

    Add files via upload

---
+++ timelock.py
@@ -0,0 +1,11 @@
+from sys import stdin 
+from datetime import datetime
+from hashlib import md5
+import pytz
+
+
+#switiching to the empty manual_datetime will turn your code
+#to use your current system time instead of a hardcoded time
+INTERVAL = 60
+MANUAL_DATETIME = "2017 04 23 18 02 30"
+#MANUAL_DATETIME = ""
