9c3524f6f914879e2573264988efca4b1755aa2d
BluJayB <80533553+BluJayB@users.noreply.github.com>
2022-11-09 11:50:23 -0600

    Add iter.bash
    This bash file goes through several steg iterations

---
+++ iter.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+ 
+# Change these lists to use the values for offset or interval
+declare -a IArray=(1 2 4 8 16 32 64)
+declare -a OArray=(1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192)
+ 
+file=$1
+
+for O in ${OArray[@]}; do
+    for I in ${IArray[@]}; do
+        python3 steg.py -r -B -o$O -i$I -w$file > temp-$O-$I.txt
+    done
+done
